25b8f549b423ab16a2ca52727c8d93c5efb1ff17
samiul2102013 <samiulhasan0186@gmail.com>
2026-06-16 04:35:36 -0700

    Fix FilteredSpectacularAPIView - use super().get()

--- spark_backend/core/views.py
+++ spark_backend/core/views.py
@@ -3,7 +3,6 @@ from django.db import connection
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from drf_spectacular.views import SpectacularAPIView
 from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from core.responses import success_response
@@ -12,21 +11,22 @@ from core.responses import success_response
 class FilteredSpectacularAPIView(SpectacularAPIView):
     filter_tag = None
 
-    def get(self, request):
-        schema = self._get_schema(request)
-        if not self.filter_tag:
-            return Response(schema)
-        paths = {}
-        for path, methods in schema.get("paths", {}).items():
-            filtered = {}
-            for method, operation in methods.items():
-                tags = operation.get("tags", [])
-                if tags and tags[0] == self.filter_tag:
-                    filtered[method] = operation
-            if filtered:
-                paths[path] = filtered
-        schema["paths"] = paths
-        return Response(schema)
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        schema = response.data
+        if self.filter_tag and isinstance(schema, dict):
+            paths = {}
+            for path, methods in schema.get("paths", {}).items():
+                filtered = {}
+                for method, operation in methods.items():
+                    tags = operation.get("tags", [])
+                    if tags and tags[0] == self.filter_tag:
+                        filtered[method] = operation
+                if filtered:
+                    paths[path] = filtered
+            schema["paths"] = paths
+            response.data = schema
+        return response
 
 
 @extend_schema_view(get=extend_schema(request=None, responses={200: None}, tags=["health"]))
