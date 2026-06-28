345282620ae5ba1af13ad61f4ff8f7a1ba792479
samiul2102013 <samiulhasan0186@gmail.com>
2026-06-27 23:51:29 -0700

    fix: id as phone number

--- spark_backend/apps/users/serializers.py
+++ spark_backend/apps/users/serializers.py
@@ -73,8 +73,6 @@ class ResetPasswordSerializer(serializers.Serializer):
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-    id = serializers.CharField(source="phone_number", read_only=True)
-
     class Meta:
         model = User
         fields = (
